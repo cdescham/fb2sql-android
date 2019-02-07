54d7a137fcb72993d697ae9725b48e7d5b2293be
Christophe Deschamps <christophe.deschamps.work@gmail.com>
2019-02-07 10:56:05 +0100

    Cleanup

--- lib/src/androidTest/java/inventivelink/com/fb2sql/ymentities/PlaceAutoComplete.kt
+++
@@ -1,7 +0,0 @@
-package fr.heymum.yoomum.bo
-
-/**
- * @author Thomas Ecalle
- * @since 2018.09.21
- */
-data class PlaceAutoComplete(val placeId:String, val addressKey:String?, val name:String?, val shortDescription:String?, val address:String?, val isAlreadyInYoomum:Boolean)
