1aacb45c66dcab93ca9ddb4d5d6776b0aa902f51
Christophe Deschamps <christophe.deschamps.work@gmail.com>
2019-02-20 08:42:27 +0100

    cleanup for 1.1.0

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+Firebase to SQL Wrapper for Android.
+
 (c) Inventivelink SARL 2019
 
-Firebase to SQL Wrapper for Android.
--- lib/src/androidTest/java/inventivelink/com/fb2sql/TestCommon.java
+++
@@ -1,8 +0,0 @@
-package inventivelink.com.fb2sql;
-
-public class TestCommon {
-
-    public static void connectSQLDatabase() {
-        SQLDatabase.getInstance().setUri("http://192.168.1.99:8000/api").setAuthToken("4UFsaVeqleSIMbvRqBfnlZ0mnE");
-    }
-}
