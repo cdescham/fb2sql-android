38fb50c7b0fbc6433787ea00d5e32db0c77f6ac9
Christophe Deschamps <christophe.deschamps.work@gmail.com>
2019-06-22 01:16:21 +0200

    Allow snapshot to be null on 404

--- lib/src/main/java/inventivelink/com/fb2sql/SQLValueEventListener.java
+++ lib/src/main/java/inventivelink/com/fb2sql/SQLValueEventListener.java
@@ -10,7 +10,7 @@ package inventivelink.com.fb2sql;
 import android.support.annotation.NonNull;
 
 public interface SQLValueEventListener {
-    void onDataChange(@NonNull SQLDataSnapshot var1);
+    void onDataChange(SQLDataSnapshot var1);
     void onCancelled(@NonNull SQLDatabaseError var1);
 }
 
