0ce01c9fee6f1e2c30eade9470535087c22b8bc8
Christophe Deschamps <christophe.deschamps.work@gmail.com>
2019-06-22 02:09:21 +0200

    Do not force Nullable

--- lib/src/main/java/inventivelink/com/fb2sql/SQLValueEventListener.java
+++ lib/src/main/java/inventivelink/com/fb2sql/SQLValueEventListener.java
@@ -11,7 +11,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 public interface SQLValueEventListener {
-    void onDataChange(@Nullable SQLDataSnapshot var1);
+    void onDataChange(SQLDataSnapshot var1);
     void onCancelled(@NonNull SQLDatabaseError var1);
 }
 
